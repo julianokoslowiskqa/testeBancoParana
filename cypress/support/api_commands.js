463b362e50e03beaa01c782c7dbfde234ffab568
julianokoslowiskqa <julianokoslowisk@gmail.com>
2023-09-11 14:23:30 -0300

    addcommandget

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,13 +1,19 @@
 // Arquivo: commands.js
 const API_URL = Cypress.env('API_BASE_URL')
 
-
 Cypress.Commands.add("customGetUsers", () => {
-    cy.api("GET", "https://jsonplaceholder.typicode.com/users")
-      .its("body")
-      .then((response) => {
-        return response;
-      });
-  });
+    cy.api({
+        method: 'GET',
+        url: `${API_URL}/1`,
+        failOnStatusCode: false,
+    }).as('response')
+    cy.get('@response').should((response)=>{
+        expect(response.status).to.equal(200),        
+        expect(response.body.name).to.equal('Leanne Graham')    
+        expect(response.body.username).to.equal('Bret')
+        expect(response.body.email).to.equal('Sincere@april.biz')    
+  })
+})
+
 
   
