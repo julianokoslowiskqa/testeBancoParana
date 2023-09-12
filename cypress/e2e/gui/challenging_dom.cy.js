8ebb57335632ed4e36ab76aa256ce78d7aa39937
julianokoslowiskqa <julianokoslowisk@gmail.com>
2023-09-12 10:30:32 -0300

    updatedata

--- cypress/e2e/gui/challenging_dom.cy.js
+++ cypress/e2e/gui/challenging_dom.cy.js
@@ -4,18 +4,18 @@ describe('Primeira parte do desafio', () => {
   beforeEach(() => {
     cy.visit('/challenging_dom')})
 
-  it('efetuar um clique em todos os 3 botões apresentados em tela.', () =>{
+  it('CT_001 efetuar um clique em todos os 3 botões apresentados em tela.', () =>{
     cy.clickbuttons()   
   })  
 
 
-  it('deve clicar em todos os botões EDIT da grid apresentada.', () => {
+  it('CT_002 deve clicar em todos os botões EDIT da grid apresentada.', () => {
     for (let rowIndex = 1; rowIndex < 10; rowIndex++) {
       cy.clickEditButtonInRow(rowIndex);
     }
   })
 
-  it('deve clicar em todos os botões DELETE da grid apresentada.', () => {
+  it('CT_003 deve clicar em todos os botões DELETE da grid apresentada.', () => {
     for (let rowIndex = 0; rowIndex < 10; rowIndex++) {
       cy.clickDeleteButtonInRow(rowIndex);
     }
